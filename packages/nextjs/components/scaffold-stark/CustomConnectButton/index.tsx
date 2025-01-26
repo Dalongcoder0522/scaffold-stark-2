e24d782e4ed44e771aa57c99ce41e88038aa7e1a
Nadai2010 <nadai2010@proton.me>
2025-01-26 18:52:33 +0000

    format

--- packages/nextjs/components/scaffold-stark/CustomConnectButton/index.tsx
+++ packages/nextjs/components/scaffold-stark/CustomConnectButton/index.tsx
@@ -43,7 +43,7 @@ export const CustomConnectButton = () => {
 
       getChainId();
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [account, status]);
 
   useEffect(() => {
